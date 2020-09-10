028bc2b392122d4785c7b194ca40ecb78bf41a6d
bleachjade <bleachjade@hotmail.com>
2020-09-10 09:52:25 +0700

    part 4 completed

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
-
-# import polls.views as views
+from django.urls import include, path
 
 urlpatterns = [
-    # path('', views.index),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
