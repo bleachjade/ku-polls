b2abc5f8f214543626924553e5b77a719552a949
bleachjade <bleachjade@hotmail.com>
2020-09-17 20:32:09 +0700

    add test for is_published and can_vote

--- polls/tests.py
+++ polls/tests.py
@@ -150,3 +150,59 @@ class QuestionModelTests(TestCase):
         time = timezone.now()
         present_question = Question(pub_date=time)
         self.assertIs(present_question.is_published(), True)
+
+    ## can_vote condition:
+    # pub_date < now < end_date == True
+    # pub_date = now(< end_date) == True
+    # pub_date > now == False
+    # now > end_date == False
+    # now = end_date == False
+    def test_can_vote_with_now_is_after_pub_date_and_before_end_date(self):
+        """
+        can_vote() return True if you're voting currently is between
+        pub_date and end_date.
+        """
+        pub_date = timezone.now() - datetime.timedelta(days=2)
+        end_date = timezone.now() + datetime.timedelta(days=10)
+        now = Question(pub_date=pub_date, end_date=end_date)
+        self.assertIs(now.can_vote(), True)
+
+    def test_can_vote_with_now_equal_to_pub_date(self):
+        """
+        can_vote() return True if you're voting currently is on
+        pub_date.
+        """
+        pub_date = timezone.now()
+        end_date = timezone.now() + datetime.timedelta(days=1)
+        now = Question(pub_date=pub_date, end_date=end_date)
+        self.assertIs(now.can_vote(), True)
+    
+    def test_can_vote_with_now_is_before_pub_date(self):
+        """
+        can_vote() return False if you're voting currently is before
+        pub_date.
+        """
+        pub_date = timezone.now() - datetime.timedelta(days=1)
+        end_date = timezone.now()
+        before = Question(pub_date=pub_date, end_date=end_date)
+        self.assertIs(before.can_vote(), False)
+
+    def test_can_vote_with_now_is_after_end_date(self):
+        """
+        can_vote() return False if you're voting currently is after
+        end_date.
+        """
+        pub_date = timezone.now() 
+        end_date = timezone.now() - datetime.timedelta(days=1)
+        before = Question(pub_date=pub_date, end_date=end_date)
+        self.assertIs(before.can_vote(), False)
+
+    def test_can_vote_with_now_is_equal_to_end_date(self):
+        """
+        can_vote() return False if you're voting currently is on
+        end_date.
+        """
+        pub_date = timezone.now() + datetime.timedelta(days=1)
+        end_date = timezone.now() 
+        before = Question(pub_date=pub_date, end_date=end_date)
+        self.assertIs(before.can_vote(), False)
